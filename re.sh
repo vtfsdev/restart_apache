8c161927f2de0427aff4a00407faa7cde075b1d3
victor tung <vtfsdev@gmail.com>
2020-10-02 11:21:13 -0500

    Bash Lab

--- re.sh
+++ re.sh
@@ -14,13 +14,15 @@ then
     # Move the current execution state to the proper directory
     cd /etc/apache2/sites-available
 
-    VHOSTS_PATH = /etc/apache2/sites-available
+    echo "$CONFIG"
+    VHOSTS_PATH = /etc/apache2/sites-available/*.conf
 
-    for "$CONFIG" not in $VHOSTS_PATH
-    do
+    if [ "$CONFIG" ne $VHOSTS_PATH ]
+    then
         #Return an error message if argument $1 is zero or empty
         echo "ERROR: File $1 does not exist in the directory"
-    done
+        exit 1
+    fi
 
     # Disable a vhost configuration
     sudo a2dissite "$CONFIG"
